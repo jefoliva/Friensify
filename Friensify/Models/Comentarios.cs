45eaf6097c7a87c693695cd1442f87de9a914dd1
jefoliva <jefodesigner@gmail.com>
2020-09-27 21:28:47 -0600

    Agrega clase Comentarios

---
+++ Friensify/Models/Comentarios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Friensify.Models
+{
+    public class Comentarios
+    {
+        public int Id { get; set; }
+        public string Nickname { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Id_publicacion { get; set; }
+    }
+}
