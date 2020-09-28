da91e9ede4b302e2bb5ca1e963b4b2153bd812c2
Erickson Enriquez <en123erickson@gmail.com>
2020-09-27 21:43:18 -0600

    Agregar clase amigos

---
+++ Friensify/Models/Amigos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Friensify.Models
+{
+    public class Amigos
+    {
+        public string Nickname_usuario { get; set; }
+        public string Nicknam_amigo { get; set; }
+        public bool Estado { get; set; }
+    }
+}
