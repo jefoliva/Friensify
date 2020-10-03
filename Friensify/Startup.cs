2f684091f40ea1eb508f2521dfbe36d1a341a8f3
Erickson Enriquez <en123erickson@gmail.com>
2020-10-02 20:31:28 -0600

    Agregar chat

--- Friensify/Startup.cs
+++ Friensify/Startup.cs
@@ -24,6 +24,7 @@ namespace Friensify
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +52,9 @@ namespace Friensify
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+               
+                endpoints.MapRazorPages();
+
             });
         }
     }
