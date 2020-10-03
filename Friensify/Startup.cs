44c4992b78e3658ddc9bc82cdf0b4fbdd7226d1a
jefoliva <jefodesigner@gmail.com>
2020-10-03 15:52:20 -0600

    Agregar Autenticación de Facebook

--- Friensify/Startup.cs
+++ Friensify/Startup.cs
@@ -25,6 +25,13 @@ namespace Friensify
         {
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddAuthentication().AddFacebook(facebookOptions =>
+            {
+                facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
+                facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+                facebookOptions.AccessDeniedPath = "/AccessDeniedPathInfo";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
