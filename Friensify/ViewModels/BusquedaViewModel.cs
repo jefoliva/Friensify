1b26179e2310f191322aafd3e34e15123a2034f6
jefoliva <jefodesigner@gmail.com>
2020-10-16 10:35:38 -0600

    Busqueda v1

---
+++ Friensify/ViewModels/BusquedaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Friensify.ViewModels
+{
+    public class BusquedaViewModel
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string URLImagen { get; set; }
+    }
+}
